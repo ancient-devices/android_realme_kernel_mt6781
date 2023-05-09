f7a5b7edb89f4dc10c3e0159bd8468ab9ccecaef
HELLINFIX <mridulsharma57570@gmail.com>
2023-05-09 08:18:09 +0000

    Update built script with an option for dirty build

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-
+clear
 function compile() 
 {
-
+echo COSMOS Kernel, CODENAMED - SPACED
+echo
+sleep 3 >/dev/null
 source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
-ccache -M 100G
+ccache -M 100G >/dev/null
 export ARCH=arm64
 export KBUILD_BUILD_HOST=COSMOS
 export KBUILD_BUILD_USER="INFIX"
@@ -13,7 +15,24 @@ git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
 git clone --depth=1 https://github.com/sarthakroy2002/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7 los-4.9-64
 git clone --depth=1 https://github.com/sarthakroy2002/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
 
-[ -d "out" ] && rm -rf AnyKernel && rm -rf out || mkdir -p out
+read -p "Wanna do dirty build? (Y/N): " build_type
+if [[ $build_type == "N" || $build_type == "n" ]]; then
+echo Deleting out directory and doing clean Build
+sleep 3 >/dev/null
+rm -rf out && mkdir -p out
+fi
+if [[ $build_type == "Y" || $build_type == "y" ]]; then
+echo Warning :- Doing dirty build
+sleep 3 >/dev/null
+fi
+if ! [[ $build_type == "Y" || $build_type == "y" ]]; then
+if ! [[ $build_type == "N" || $build_type == "n" ]]; then
+echo Invalid Input , Read carefully before typing
+echo Trying to restart script
+sleep 3 >/dev/null
+. build.sh && exit
+fi
+fi
 
 make O=out ARCH=arm64 spaced_defconfig
 
